d451b569b26fda65fa16a05e674f3663855f3af0
Roshan09876 <khadkaroshan715@gmail.com>
2022-12-22 11:35:05 +0545

    laundry

---
+++ lib/laundry.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+class laundry extends StatefulWidget {
+  const laundry({super.key});
+
+  @override
+  State<laundry> createState() => _laundryState();
+}
+
+class _laundryState extends State<laundry> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        height: 5000,
+        width: 5000,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 99, 67, 55),
+            Color.fromARGB(175, 255, 255, 255)
+            // Color(0xFF701Ebd),
+            // Color(0xFF873bc),
+            // Color(0xFFfe4a97)
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 90),
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.only(right: 200),
+              child: Text(
+                "Laundry",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                    color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 100),
+              child: Container(
+                child: Text(
+                  "Schedules",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 110,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 13, top: 8, bottom: 8),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 120,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Sunday",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Container(
+                        child: Text(
+                          "Colored Clothes",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 13, top: 8, bottom: 8),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(
+                          "Bed Sheets",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 130,
+                      ),
+                      Container(
+                        width: 120,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Tuesday",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 13, top: 8, bottom: 8),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 120,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Friday",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Container(
+                        child: Text(
+                          "White Clothes",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
