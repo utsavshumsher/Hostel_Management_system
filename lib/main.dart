dfb63965cc17c3a24ccfcec5ccd566c92e15730b
utsav thapa <utsavshumsherthapa@gmail.com>
2023-01-04 13:24:43 +0545

    t

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:sleepholic/Laundry/laundry.dart';
-import 'package:sleepholic/Rooms/one_seater.dart';
 
+
+import 'package:sleepholic/dashboard.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,9 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
       debugShowCheckedModeBanner: false,
-      home: One_Seater(),
+      home: Dashboard(),
     );
   }
 }
