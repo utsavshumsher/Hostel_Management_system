8cf51d5176aa3050e9e2c4b6bf6a013a969c6132
sneehaa <adhikarisneha0001@gmail.com>
2023-01-04 14:58:58 +0545

    changes

---
+++ lib/screens/Welcome/welcome_screen.dart
@@ -0,0 +1,81 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:sleepholic/screens/Register/register_screen.dart';
+import 'package:sleepholic/screens/login/loginScreen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  final form = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    var Height = MediaQuery.of(context).size.height;
+    return Container(
+      height: Height,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/Sleepholic.jpg"),
+            fit: BoxFit.cover
+        ),
+      ),
+
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              height: Height/100*500,
+            ),
+            ElevatedButton(onPressed: () {
+              // EdgeInsets.symmetric(horizontal: 90);
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => RegisterScreen(),
+                ),
+              );
+            },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor: Color.fromRGBO(93, 108, 137, 1.0),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 8)
+
+                ),
+                child: Text("Join Now")),
+
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => LoginScreen(),
+                ),
+              );
+            },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    backgroundColor: Color.fromRGBO(93, 108, 137, 1.0),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 35, vertical: 8)
+                ),
+                child: Text("Login"))
+          ],
+
+
+        ),
+      ),
+
+    );
+  }
+}
--- lib/screens/login/loginScreen.dart
+++ lib/screens/login/loginScreen.dart
@@ -1,15 +1,156 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFFFF6EA),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Image.asset(
+                      "assets/images/MAKE YOURSELF ATA HOME.jpg",
+                      height: 250,
+                      width: 250,
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "Login",
+                      style:
+                          TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Email',
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      // margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            // label: Text("Email"),
+                            hintText: "Please enter your email",
+                            prefixIcon: Icon(Icons.accessibility_outlined)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Password',
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      // margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            // label: Text("Password"),
+                            hintText: "Please enter your password",
+                            prefixIcon: Icon(Icons.visibility)),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(color: Colors.redAccent),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 340,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                  child: Text("Login"),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: "Dont have account? ",
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "Sign Up!",
+                      style: const TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 18,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
