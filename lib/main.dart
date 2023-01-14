efd9d63971aa160723e46ff53991d16ce4036e9a
Roshan09876 <khadkaroshan715@gmail.com>
2023-01-14 17:05:35 +0545

    fire

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:sleepholic/ContactUs/ContactUs.dart';
@@ -17,8 +20,10 @@ import 'package:sleepholic/screens/login/login_screen.dart';
 import 'Foods/swipe.dart';
 import 'RulesScreen/rules.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -33,12 +38,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: GoogleFonts.alike().fontFamily,
       ),
-      home: Profile(),
+      home: LoginScreen(),
+    );
+  }
+}
 
       // routes: {
       //   "/login": (BuildContext context) => LoginScreen(),
       //   "register": (BuildContext context) => RegisterScreen(),
       // }
-    );
-  }
-}
