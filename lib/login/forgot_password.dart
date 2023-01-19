cd444194d3452a20723ac7ba936aa3524defa315
swarooprana7 <swarooprana77@gmail.com>
2023-01-19 10:09:19 +0545

    forgot image added

--- lib/login/forgot_password.dart
+++ lib/login/forgot_password.dart
@@ -15,95 +15,110 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFF6EA),
-      appBar: AppBar(
-          title: Center(
-        child: Text('Forgot Password'),
-      )),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                child: Image.asset(
-                  "assets/images/logo.png",
-                  height: 240,
-                  width: 240,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Image.asset(
+                        "assets/images/logo.png",
+                        height: 260,
+                        width: 200,
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        "Forgot \n password ",
+                        style: TextStyle(
+                            fontSize: 35, fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Container(
-                child: Text(
-                  "Forgot password",
-                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.normal),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Email',
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  String pattern =
-                      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-                      r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
-                      r"{0,253}[a-zA-Z0-9])?)*$";
-                  RegExp regex = RegExp(pattern);
-                  if (value == null || value.isEmpty || !regex.hasMatch(value))
-                    return 'Enter a valid email address';
-                  else
-                    return null;
-                  //return null;
-                },
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[350],
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    // label: Text("Email"),
-                    hintText: "Please enter your email",
-                    prefixIcon: Icon(Icons.email_outlined)),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              // ElevatedButton(
-              //   child: Text('Forgot'),
-              //   onPressed: () {},
-              // ),
-              Container(
-                width: 400,
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Validation success')),
-                      );
-
-                      // Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //         builder: (context) => const OTPScreen()));
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Unsuccessfull ')),
-                      );
-                    }
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    String pattern =
+                        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+                        r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+                        r"{0,253}[a-zA-Z0-9])?)*$";
+                    RegExp regex = RegExp(pattern);
+                    if (value == null ||
+                        value.isEmpty ||
+                        !regex.hasMatch(value))
+                      return 'Enter a valid email address';
+                    else
+                      return null;
+                    //return null;
                   },
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(21, 34, 56, 1.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      )),
-                  child: Text("Forgot Password"),
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[350],
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      // label: Text("Email"),
+                      hintText: "Please enter your email",
+                      prefixIcon: Icon(Icons.email_outlined)),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 40,
+                ),
+                // ElevatedButton(
+                //   child: Text('Forgot'),
+                //   onPressed: () {},
+                // ),
+                Container(
+                  width: 400,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Validation success')),
+                        );
+
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => const OTPScreen()));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Unsuccessfull ')),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(21, 34, 56, 1.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        )),
+                    child: Text("Forgot Password"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
