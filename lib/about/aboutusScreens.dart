edc77aaa0122904f6511c73dca8e37bf9e6dd39f
swarooprana7 <swarooprana77@gmail.com>
2023-02-02 11:38:25 +0545

    :hammer: changes on about us

--- lib/about/aboutusScreens.dart
+++ lib/about/aboutusScreens.dart
@@ -27,7 +27,107 @@ class _AboutUsPageState extends State<AboutUsPage> {
         centerTitle: true,
         title: Text("About Us"),
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0, 8),
+                      blurRadius: 8,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: 200,
+                        viewportFraction: 1.0,
+                        enlargeCenterPage: false,
+                        autoPlay: true,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _current = index;
+                          });
+                        },
+                      ),
+                      items: [
+                        Image.asset("assets/images/img_4.png"),
+                        Image.asset("assets/images/img.png"),
+                        Image.asset("assets/images/img_1.png"),
+                        Image.asset("assets/images/img_2.png"),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (int i = 0; i < 3; i++)
+                          i == _current
+                              ? _buildDot(context, true)
+                              : _buildDot(context, false),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: Text(
+                        "Sleepholic",
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "This app is developed for managing hostel operations such as room allocation, fee management, and other activities. It aims to provide a seamless and efficient experience for both hostel staff and residents.",
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    SizedBox(height: 16),
+                    Container(
+                      width: 400,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrangeAccent,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: MaterialButton(
+                        // color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          // Open the company website or email
+                        },
+                        child: Text("Contact Us"),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    Center(
+                      child: Text(
+                        "Our Location",
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Image.asset("assets/images/img_3.png"),
+                    SizedBox(height: 32),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
