832ac188f129ad1f4b3ffc56002d76cd42dff87d
swarooprana7 <swarooprana77@gmail.com>
2023-02-02 10:55:51 +0545

    calender updated

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: GoogleFonts.alike().fontFamily,
       ),
-      home: complain(),
+      home: AboutUsPage(),
     );
   }
 }
